bafa50846d93092d6fcee3462d6ae160eb6533fe
shijiao <drremain@crew4dance.com>
2021-11-08 20:22:43 +0800

    feat: register

--- main.go
+++ main.go
@@ -1,7 +1,107 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"varchar(11);not null;unique"`
+	Password  string `gorm:"size:255;not null"`
+}
 
 func main() {
-	fmt.Println("helloworld")
+	db := InitDB()
+
+	r := gin.Default()
+	r.POST("/api/auth/register", func(c *gin.Context) {
+		// 获取参数
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
+		// 数据验证
+		if len(telephone) != 11 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号不正确"})
+			return
+		}
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+			return
+		}
+		// 如果名称没有传，给一个10位的随机字符串
+		if len(name) == 0 {
+			name = RandomString(10)
+		}
+
+		log.Println(name, telephone, password)
+		// 判断手机号是否存在
+		if isTelephoneExist(db, telephone) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
+			return
+		}
+
+		// 创建用户
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
+
+		// 返回结果
+		c.JSON(http.StatusOK, gin.H{
+			"message": "注册成功",
+		})
+	})
+	r.Run(":8080")
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+
+	return false
+}
+
+func RandomString(n int) string {
+	var letters = []byte("FabTLnBA9MNA3P6DgHWJeNdzavznDY5AIwk7f3s7FD0=")
+	result := make([]byte, n)
+
+	rand.Seed(time.Now().UnixNano())
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	host := "192.168.50.143"
+	port := "3306"
+	database := "go_vue"
+	username := "root"
+	password := "root"
+	charset := "utf8mb4"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Local", username, password, host, port, database, charset)
+
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database, err: " + err.Error())
+	}
+
+	db.AutoMigrate(&User{})
+
+	return db
 }
