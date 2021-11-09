3b185cc3c00d2a02d307b6d38ef4a0899aa9694f
shijiao <drremain@crew4dance.com>
2021-11-09 16:25:03 +0800

    feat: finish server

--- common/database.go
+++ common/database.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"gitlab.com/remainlab/go-vue/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,12 +11,12 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	host := "192.168.50.143"
-	port := "3306"
-	database := "go_vue"
-	username := "root"
-	password := "root"
-	charset := "utf8mb4"
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Local", username, password, host, port, database, charset)
 
 	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
